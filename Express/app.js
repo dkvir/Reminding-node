aa36fa898ba6015aab5ce31084d63a45cc9a0f3f
dkvir <kvirika137@gmail.com>
2024-04-26 13:58:51 +0400

    📦 Express: Using router

--- Express/app.js
+++ Express/app.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const app = express()
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/add-product', (req,res,next)=>{
-    res.send('<form action="./product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
 
-app.post('/product', (req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello From Express! </h1>');
-})
 app.listen(4000);
