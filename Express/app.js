26e129366b01557daed2f197d8a80e77a4a7ac11
dkvir <kvirika137@gmail.com>
2024-05-02 14:13:11 +0400

    📦 NEW: 404 page && rootdir utility

--- Express/app.js
+++ Express/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser');
@@ -11,7 +13,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(4000);
