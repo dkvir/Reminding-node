f08f37415947205fb361b8f6626dbd9974ec44be
dkvir <kvirika137@gmail.com>
2024-06-04 15:36:15 +0400

    📦 NEW: Pug configuration for dynamic html

--- Express/app.js
+++ Express/app.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const app = express();
-
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, "views"));
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
--- Express/routes/shop.js
+++ Express/routes/shop.js
@@ -4,9 +4,12 @@ const path = require('path');
 const rootDir = require('../utils/path')
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir, '/views', 'shop.html'));
+    // res.sendFile(path.join(rootDir, '/views', 'shop.html'));
+    res.render('shop')
+
 })
 
 module.exports = router
