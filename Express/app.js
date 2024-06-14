c67cafc35f774426e4230a8f271bb7d4ebbb0706
dkvir <kvirika137@gmail.com>
2024-06-14 16:10:24 +0400

    👌 IMPROVE: completed pug with node

--- Express/app.js
+++ Express/app.js
@@ -19,7 +19,7 @@ app.use(shopRoutes);
 
 app.use((req,res,next)=>{
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-    res.render('404', {docTitle:'Error Page'})
+    res.render('404', {docTitle:'Page Not Found!'})
 })
 
 app.listen(4000);
