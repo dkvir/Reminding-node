6ec664c68cdbb121804a4597e8020e9fd11ea92d
dkvir <kvirika137@gmail.com>
2024-07-04 15:14:42 +0400

    :zap: 📦 NEW: completed ejs templating engine configuration and using instead of html

--- Express/app.js
+++ Express/app.js
@@ -1,25 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 
 const app = express();
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-    res.render('404', {docTitle:'Page Not Found!'})
-})
+app.use((req, res, next) => {
+  res.render("404", { docTitle: "Page Not Found!", path: "" });
+});
 
 app.listen(4000);
